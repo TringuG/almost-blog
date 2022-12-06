acdacaf582a519c3b3c16ed0a9f9496f03f3c881
TringuG <richardnalepka@gmail.com>
2022-12-06 09:27:45 +0000

    up

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,11 @@
 <script>
-    import "../app.css";
-  </script>
+    import "../app.css"; 
+</script>
   
 
   <nav class="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded drop-shadow-md">
     <div class="container flex flex-wrap items-center justify-between mx-auto">
-      <a href="#" class="flex items-center">
+      <a href="/" class="flex items-center">
           <span class="self-center text-xl font-semibold whitespace-nowrap">Almost Blog</span>
       </a>
       <button data-collapse-toggle="navbar-default" type="button" class="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200" aria-controls="navbar-default" aria-expanded="false">
@@ -15,16 +15,16 @@
       <div class="hidden w-full md:block md:w-auto" id="navbar-default">
         <ul class="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-4 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white">
           <li>
-            <a href="#" class="block py-2 pl-3 pr-4 text-gray-700 rounded p-0">Daily Digest</a>
+            <a href="daily" class="block py-2 pl-3 pr-4 text-gray-700 rounded p-0">Daily Digest</a>
           </li>
           <li>
-            <a href="#" class="block py-2 pl-3 pr-4 text-gray-700 rounded p-0">Design Tools</a>
+            <a href="design" class="block py-2 pl-3 pr-4 text-gray-700 rounded p-0">Design Tools</a>
           </li>
           <li>
-            <a href="#" class="block py-2 pl-3 pr-4 text-gray-700 rounded">Tutorials</a>
+            <a href="tutorials" class="block py-2 pl-3 pr-4 text-gray-700 rounded">Tutorials</a>
           </li>
           <li>
-            <a href="#" class="block py-2 pl-3 pr-3 text-white bg-gray-700 rounded text-center">Subscribe</a>
+            <a href="newsletter"class="block py-2 pl-3 pr-3 text-white bg-gray-700 rounded text-center">Subscribe</a>
           </li>
         </ul>
       </div>
@@ -34,3 +34,5 @@
 <body class="bg-gray-100">
   <slot />
 </body>
+
+
---
+++ src/routes/newsletter/+page.svelte
@@ -0,0 +1,18 @@
+      <div id="modal" class="flex items-center justify-center py-12">
+          <div class="bg-white dark:bg-gray-800 dark:border-transparent border rounded-md mx-4 md:w-10/12 flex items-center justify-center relative">
+              <div class="relative z-10 w-full">
+                  <div class="flex flex-col justify-center pt-10 pb-12 px-4 md:px-10">
+                      <div class="absolute top-5 right-5">
+                      </div>
+                      <h1 class="pt-4 pb-10 text-2xl md:text-4xl lg:text-5xl font-bold text-center text-gray-800 dark:text-white ">Get new products every months in your inbox!</h1>
+                      <p class="text-sm leading-none dark:text-white text-center md:pt-0 pt-3">Try free for 30 days - <u class="no-underline text-indigo-700 dark:text-indigo-400">No credit card required</u></p>
+                      <div class="flex justify-center flex-col md:flex-row gap-4 md:gap-7">
+                          <input type="email" placeholder="Enter you email to join" class="bg-white dark:bg-gray-600 dark:placeholder-gray-200 dark:border-gray-700 dark:text-white border rounded-md placeholder-gray-300 border-gray-300 p-4 w-full lg:w-1/2 focus:outline-none" />
+                          <button class="px-8 py-4 bg-indigo-700 rounded-md text-white font-medium text-base focus:outline-none focus:ring-2 focus:ring-indigo-800 focus:ring-opacity-50 hover:bg-indigo-800 w-full md:w-auto">Subscribe</button>
+                      </div>
+                  </div>
+              </div>
+          </div>
+      </div>
+    
+  
